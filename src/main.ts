d024c5402849b6dd181dd58dca3f80086401d8c6
dipto <kmmahbub@proton.me>
2023-03-15 19:04:55 +0900

    intial setup

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import * as path from 'path';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.setGlobalPrefix('api');
+  app.useStaticAssets({
+    root: path.join(__dirname, '..', 'public'),
+    prefix: '/',
+  });
+  await app.listen(3000);
+}
+bootstrap();
